11e9550f3925bac55b7a2107358490660519cb5f
bingyusandor <bingyusandor@gmail.com>
2021-12-29 21:56:14 +0100

    Project created

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("bapt.certzbaptcert.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("bapt.certzbaptcert.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
